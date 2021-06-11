30b923ea10e4e03b72f1c75ddd2b2721f5425117
BenjiFischman <fisch885@umn.edu>
2021-06-10 19:59:38 -0700

    href link

--- client/src/sections/home.jsx
+++ client/src/sections/home.jsx
@@ -6,7 +6,9 @@ const Home = () => {
   return (
     <div className="App">
       <div className="Container Home">
+          <a href={"https://www.webex.com/"}>
         <img className="img " src={imgLogo} alt="new Webex logo who dis?"></img>
+        </a>
       </div>
     </div>
   );
